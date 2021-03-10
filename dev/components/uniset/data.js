70eb818a44fc8a502638715962238293d4e25f39
github-action-benchmark <github@users.noreply.github.com>
2021-03-10 21:52:32 +0000

    add Rust Benchmark (cargo) benchmark result for 2d6664a376df209f86d4af47f0a686b91b8172e0

--- dev/components/uniset/data.js
+++ dev/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614277251603,
+  "lastUpdate": 1615413152671,
   "repoUrl": "https://github.com/aethanyc/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -2129,6 +2129,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 33766163,
             "range": "± 524436",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2d6664a376df209f86d4af47f0a686b91b8172e0",
+          "message": "Move DTF to only store DateSymbols and a single pattern. (#518)",
+          "timestamp": "2021-03-05T07:40:03-08:00",
+          "tree_id": "c6e88347fda54bd0e73aba4c94510803800e4025",
+          "url": "https://github.com/aethanyc/icu4x/commit/2d6664a376df209f86d4af47f0a686b91b8172e0"
+        },
+        "date": 1615413152123,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 37359854,
+            "range": "± 397060",
             "unit": "ns/iter"
           }
         ]
