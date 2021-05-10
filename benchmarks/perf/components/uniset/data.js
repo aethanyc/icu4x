c9ad5926e3f97509ddb40f567e1201c434f1688a
github-action-benchmark <github@users.noreply.github.com>
2021-05-10 16:34:09 +0000

    add Rust Benchmark (cargo) benchmark result for 8a91a7c33f5bb56f52c198161a4a4c3a117583a9

---
+++ benchmarks/perf/components/uniset/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1620664449535,
+  "repoUrl": "https://github.com/aethanyc/icu4x",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8a91a7c33f5bb56f52c198161a4a4c3a117583a9",
+          "message": "Require GCP key to build docs preview (#708)",
+          "timestamp": "2021-05-08T13:19:59-05:00",
+          "tree_id": "0595fdc51c3a25f889e671b09e13dd8facc63db6",
+          "url": "https://github.com/aethanyc/icu4x/commit/8a91a7c33f5bb56f52c198161a4a4c3a117583a9"
+        },
+        "date": 1620664448827,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 34972036,
+            "range": "± 999093",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
