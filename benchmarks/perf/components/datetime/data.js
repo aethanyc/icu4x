91268ca97e71d97ddf36f9ed77ab83a56b84fa40
github-action-benchmark <github@users.noreply.github.com>
2021-05-10 16:34:51 +0000

    add Rust Benchmark (cargo) benchmark result for 8a91a7c33f5bb56f52c198161a4a4c3a117583a9

---
+++ benchmarks/perf/components/datetime/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1620664491077,
+  "repoUrl": "https://github.com/aethanyc/icu4x",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8a91a7c33f5bb56f52c198161a4a4c3a117583a9",
+          "message": "Require GCP key to build docs preview (#708)",
+          "timestamp": "2021-05-08T13:19:59-05:00",
+          "tree_id": "0595fdc51c3a25f889e671b09e13dd8facc63db6",
+          "url": "https://github.com/aethanyc/icu4x/commit/8a91a7c33f5bb56f52c198161a4a4c3a117583a9"
+        },
+        "date": 1620664490437,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_overview",
+            "value": 1439605,
+            "range": "± 133617",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 2748822,
+            "range": "± 192414",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
